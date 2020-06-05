8dfd98a6c71b73430a1baa445294b2f547ae28fe
imyp <intipagaza@live.dk>
2020-06-05 09:37:49 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hmmlogo",
+    version="0.0.2",
+    author="Inti Manuel Yabar-Pagaza",
+    author_email="intipagaza@live.dk",
+    description="Package to plot logos for profile hidden Markov models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/imyp/hmmlogo",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'matplotlib',
+        'requests',
+        'pandas',
+        'numpy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
